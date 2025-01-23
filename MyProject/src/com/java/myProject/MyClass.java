e37c7660e6689abcbd0510feebfb2c3c719466b3
ATANU <ATANU@LAPTOP-5SN02KF4.lan>
2025-01-23 15:50:48 +0530

    initial commit

---
+++ MyProject/src/com/java/myProject/MyClass.java
@@ -0,0 +1,20 @@
+package com.java.myProject;
+
+public class MyClass 
+{
+	public static void main(String args[])
+	{
+		System.out.print("hello   ");
+		hello();
+	}
+	public static void hello()
+	{
+		int num=10;
+		for(int i=1;i<=2445;i++)
+		{
+			num=num+i;
+		}
+		System.out.print("hello !!");
+	}
+	
+}
---
+++ MyProject/src/com/java/myProject/Subho.java
@@ -0,0 +1,10 @@
+package com.java.myProject;
+
+public class Subho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("heyy !! Subhooo");
+	}
+
+}
